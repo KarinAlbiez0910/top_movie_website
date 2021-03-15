2ecd0e7ee0739f8227f792d24475af0eae4cd340
Your Name <you@example.com>
2021-03-15 23:22:03 +0100

    reverted to state 71e44baaf307900b705daff30a4b26b406849bca

--- main.py
+++ main.py
@@ -5,12 +5,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
-import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///blog.db")
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///my-movies-collection.db"
 Bootstrap(app)
 db = SQLAlchemy(app)
 
